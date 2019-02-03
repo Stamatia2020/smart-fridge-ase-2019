65dd74f8368e5295d1d561faa97e0b932803e67a
Vasilis Gaitanidis <vasilis.gaitanidis@gmail.com>
2019-02-03 22:02:23 +0200

    Changed UI classes to view package.

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,24 +1,19 @@
-
+package view;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import java.awt.Font;
-import java.awt.Image;
 import java.awt.Color;
-import java.awt.event.ActionListener;
+import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
-import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import java.awt.Toolkit;
+import javax.swing.border.EmptyBorder;
 
 public class MainFrame extends JFrame {
 
@@ -47,7 +42,7 @@ public class MainFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public MainFrame() {
-		
+
 		this.setTitle("Smart Fridge");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 740, 510);
@@ -55,11 +50,11 @@ public class MainFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.WHITE);
 		contentPane.add(panel, BorderLayout.CENTER);
-		
+
 		profileButton = new JButton("My Profile");
 		profileButton.setForeground(Color.WHITE);
 		profileButton.addActionListener(new ActionListener() {
@@ -68,53 +63,47 @@ public class MainFrame extends JFrame {
 		});
 		profileButton.setBackground(new Color(30, 144, 255));
 		profileButton.setFont(new Font("Perpetua Titling MT", Font.PLAIN, 18));
-		
+
 		exitButton = new JButton("Exit");
-		
+
 		exitButton.setForeground(Color.WHITE);
 		exitButton.setBackground(new Color(30, 144, 255));
 		exitButton.setFont(new Font("Perpetua Titling MT", Font.PLAIN, 18));
-		
+
 		fridgeButton = new JButton("MY FRIDGE");
 		fridgeButton.setFont(new Font("Perpetua Titling MT", Font.PLAIN, 18));
 		fridgeButton.setForeground(new Color(255, 255, 255));
 		fridgeButton.setBackground(new Color(30, 144, 255));
 
 		GroupLayout gl_panel = new GroupLayout(panel);
-		gl_panel.setHorizontalGroup(
-			gl_panel.createParallelGroup(Alignment.TRAILING)
-				.addGroup(gl_panel.createSequentialGroup()
-					.addGap(220)
-					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
-						.addComponent(fridgeButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
-						.addComponent(profileButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-					.addGap(192))
-				.addGroup(Alignment.LEADING, gl_panel.createSequentialGroup()
-					.addGap(533)
-					.addComponent(exitButton, GroupLayout.PREFERRED_SIZE, 134, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(45, Short.MAX_VALUE))
-		);
-		gl_panel.setVerticalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel.createSequentialGroup()
-					.addGap(179)
-					.addComponent(profileButton, GroupLayout.PREFERRED_SIZE, 47, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(fridgeButton, GroupLayout.PREFERRED_SIZE, 47, GroupLayout.PREFERRED_SIZE)
-					.addGap(58)
-					.addComponent(exitButton, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(73, Short.MAX_VALUE))
-		);
+		gl_panel.setHorizontalGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panel.createSequentialGroup().addGap(220)
+						.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+								.addComponent(fridgeButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 300,
+										Short.MAX_VALUE)
+								.addComponent(profileButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+						.addGap(192))
+				.addGroup(Alignment.LEADING,
+						gl_panel.createSequentialGroup().addGap(533)
+								.addComponent(exitButton, GroupLayout.PREFERRED_SIZE, 134, GroupLayout.PREFERRED_SIZE)
+								.addContainerGap(45, Short.MAX_VALUE)));
+		gl_panel.setVerticalGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup().addGap(179)
+						.addComponent(profileButton, GroupLayout.PREFERRED_SIZE, 47, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(fridgeButton, GroupLayout.PREFERRED_SIZE, 47, GroupLayout.PREFERRED_SIZE)
+						.addGap(58).addComponent(exitButton, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(73, Short.MAX_VALUE)));
 		panel.setLayout(gl_panel);
 	}
-	
+
 	private void createEvents() {
 		profileButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+
 			}
 		});
-		
-		
+
 	}
 }
