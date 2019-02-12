ca913e1ab97a1ae0605d2086090ae28f1742da08
lephleg <lephleg@gmail.com>
2019-02-12 21:42:30 +0200

    Design FoodDao interface, Add FoodDao implementation.

--- src/main/java/dao/FoodDao.java
+++ src/main/java/dao/FoodDao.java
@@ -1,5 +1,53 @@
 package main.java.dao;
 
+import main.java.dto.Food;
+
+import java.util.List;
+
 public interface FoodDao {
+
+    /**
+     * Add a food.
+     *
+     * @param food the given food
+     */
+    void add(Food food);
+
+    /**
+     * Get all foods.
+     *
+     * @return all foods
+     */
+    List<Food> getAll();
+
+    /**
+     * Get a food by an identifier.
+     *
+     * @param id the given identifier
+     * @return a food by its identifier.
+     */
+    Food getById(int id);
+
+    /**
+     * Get a food by its name.
+     *
+     * @param name the given name
+     * @return a food by its identifier.
+     */
+    Food getByName(String name);
+
+    /**
+     * Update an existing food.
+     *
+     * @param food the given food
+     */
+    void update(Food food);
+
+    /**
+     * Delete a food.
+     *
+     * @param food the given food
+     */
+    void delete(Food food);
 
 }
--- src/main/java/dao/impl/FoodDaoImpl.java
+++ src/main/java/dao/impl/FoodDaoImpl.java
@@ -1,7 +1,70 @@
 package main.java.dao.impl;
 
 import main.java.dao.FoodDao;
+import main.java.dto.Food;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository("foodDao")
+@Transactional
 public class FoodDaoImpl implements FoodDao {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    /**
+     * Save the food in the database.
+     */
+    @Override
+    public void add(Food food) {
+        entityManager.persist(food);
+    }
+
+    /**
+     * Return all the foods stored in the database.
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<Food> getAll() {
+        return entityManager.createQuery("from food").getResultList();
+    }
+
+    /**
+     * Return the food having the passed id.
+     */
+    @Override
+    public Food getById(int id) {
+        return entityManager.find(Food.class, id);
+    }
+
+    /**
+     * Return the food having the passed name.
+     */
+    @Override
+    public Food getByName(String name) {
+        return entityManager.find(Food.class, name);
+    }
+
+    /**
+     * Update the passed food in the database.
+     */
+    @Override
+    public void update(Food food) {
+        entityManager.merge(food);
+    }
+
+    /**
+     * Delete the food from the database.
+     */
+    @Override
+    public void delete(Food food) {
+        if (entityManager.contains(food))
+            entityManager.remove(food);
+        else
+            entityManager.remove(entityManager.merge(food));
+    }
 }
