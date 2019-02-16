37fa95a096382299053e959b4c61d0774df46d98
Vasilis Gaitanidis <vasilis.gaitanidis@gmail.com>
2019-02-16 23:05:46 +0200

    Implement FoodControllerImpl

--- src/main/java/controller/impl/FoodControllerImpl.java
+++ src/main/java/controller/impl/FoodControllerImpl.java
@@ -2,36 +2,37 @@ package main.java.controller.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import main.java.controller.FoodController;
+import main.java.dao.FoodDao;
 import main.java.dto.Food;
 
 @Controller
 public class FoodControllerImpl implements FoodController {
 
+	@Autowired
+	private FoodDao foodDao;
+	
 	@Override
 	public void add(Food food) {
-		// TODO Auto-generated method stub
-		
+		foodDao.add(food);
 	}
 
 	@Override
 	public List<Food> listFoods() {
-		// TODO Auto-generated method stub
-		return null;
+		return foodDao.getAll();
 	}
 
 	@Override
 	public void update(Food food) {
-		// TODO Auto-generated method stub
-		
+		foodDao.update(food);
 	}
 
 	@Override
 	public void delete(Food food) {
-		// TODO Auto-generated method stub
-		
+		foodDao.delete(food);
 	}
 
 }
