6f344aed665c5b4a5d41c0e853148c08fcc83de7
lephleg <lephleg@gmail.com>
2019-02-16 17:08:23 +0200

    Fix bug with older listeners firing altogether.

--- src/main/java/ui/controllers/AbstractFrameController.java
+++ src/main/java/ui/controllers/AbstractFrameController.java
@@ -8,7 +8,16 @@ abstract public class AbstractFrameController {
     abstract public void prepareAndShowFrame();
 
     protected void registerAction(JButton button, ActionListener listener) {
+        removeExistingListeners(button);
         button.addActionListener(listener);
+    }
+
+    private void removeExistingListeners(JButton button) {
+        ActionListener[] listeners = button.getActionListeners();
+
+        for (ActionListener l : listeners) {
+            button.removeActionListener(l);
+        }
     }
 
 }
